b45248ce8c2f5a086236ec441284b4f34bbea85a
imhasibulhasan <hashibul218@gmail.com>
2021-08-12 00:41:43 +0600

    Show data  from database

---
+++ inc/data.php
@@ -0,0 +1,13 @@
+<?php
+
+$conn = new mysqli("localhost","root","","vue_curd");
+
+$data = $conn->query("SELECT * FROM users ORDER By id DESC");
+
+$all_users = [];
+
+while( $users = $data->fetch_assoc() ){
+    array_push($all_users, $users);
+}
+
+echo json_encode($all_users);
